deaf3c857f657f5d89ae053e3ab4a0cd0930ab66
Meet Madhu <meet.madhu@MBProC-MeetM.hitronhub.home>
2021-11-24 17:57:31 -0800

    api keys remove

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,8 +2,8 @@ import { Md5 } from 'ts-md5/dist/md5';
 
 //data needed for hash
 const url = 'https://gateway.marvel.com:443/v1/public';
-const publicKey = '89dd9f6152897008a59e0050cecf5713';
-const privateKey = 'e3e2db831d87d93f01ede25bfdc6bd0d46ac9fa6';
+const publicKey = 'YOUR_PUBLIC_KEY';
+const privateKey = 'YOUR_PRIVATE_KEY';
 const timeStamp = Date.now();
 
 //hash creation
